89301afef35c3f125d2d58c5ccb695196c0ee153
Marius-Cristian Cristescu <cristescu_m@yahoo.com>
2023-11-17 21:05:19 +0200

    Update flyway folder

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -3,12 +3,11 @@ create table ingredients (
     name varchar(50) not NULL,
     stock float,
     allergens varchar(255)
-)
-
+);
 
 create table pizzas (
     id SERIAL primary key,
     name varchar(50) not NULL,
     allergens varchar(255),
-    stock float,
+    price float
 );
