21f8bfc5432c3ed3d2948fbad0c662cd293bce8d
Marius-Cristian Cristescu <cristescu_m@yahoo.com>
2023-11-22 12:35:24 +0200

    Update profiles

---
+++ src/main/java/com/pizza/config/CloudConfig.java
@@ -0,0 +1,18 @@
+package com.pizza.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("cloud")
+public class CloudConfig {
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().username(System.getenv("DB_USER")).password(System.getenv("DB_PASSWORD")).url(System.getenv("DB_URL"))
+                .driverClassName("org.postgresql.Driver").build();
+    }
+}
