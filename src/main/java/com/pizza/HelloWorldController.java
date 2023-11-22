2d96d86d21f3da83dc0a287e1832f8ae436ea32d
Marius-Cristian Cristescu <cristescu_m@yahoo.com>
2023-11-22 13:56:41 +0200

    Update profiles part 7

--- src/main/java/com/pizza/HelloWorldController.java
+++ src/main/java/com/pizza/HelloWorldController.java
@@ -2,12 +2,20 @@ package com.pizza;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloWorldController
 {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        Map<String, String> envMap = System.getenv();
+        String s = "";
+        for (String envName : envMap.keySet()) {
+            //System.out.format("%s = %s%n", envName, envMap.get(envName));
+            s+= envName + "--" +envMap.get(envName) + "<br>";
+        }
+        return "Hello, World!" + "\n"+s;
     }
 }
 
