b10d1a98e0850f54aef51ad5e0378c5cb78405ff
Marius-Cristian Cristescu <cristescu_m@yahoo.com>
2023-12-20 15:52:10 +0200

    Add CORS mapping for react app

--- src/main/java/com/pizza/config/WebSecurityConfig.java
+++ src/main/java/com/pizza/config/WebSecurityConfig.java
@@ -19,13 +19,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity
         (securedEnabled = true,
                 jsr250Enabled = true,
                 prePostEnabled = true)
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
     @Autowired
     @Lazy
     UserDetailsManagerImpl userDetailsManager;
@@ -90,5 +92,14 @@ public class WebSecurityConfig {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
